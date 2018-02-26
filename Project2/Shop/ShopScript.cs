b625664d8a0249f12e6dd1556bda45be63611a07
lazorkitten <drkatter97@gmail.com>
2018-02-26 11:28:10 -0500

    Add files via upload

---
+++ Project2/Shop/ShopScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopScript : MonoBehaviour {
+
+    public GameObject shopCanvas;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triggered");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("triggered by player");
+            OpenShop();
+        }
+    }
+
+    void OpenShop()
+    {
+        shopCanvas.SetActive (true);
+        Time.timeScale = 0;
+    }
+
+    public void CloseShop()
+    {
+        shopCanvas.SetActive (false);
+        Time.timeScale = 1;
+    }
+}
