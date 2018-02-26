e7cd59c9c03a146b6d3240bd2f49722c93e56439
Nick Stokowski <nkstokowski@gmail.com>
2018-02-25 23:02:13 -0500

    Minor movement updates

--- Project2/Assets/_Scripts/Player/PlayerMovement.cs
+++ Project2/Assets/_Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	[HeaderAttribute("Movement Variables")]
+    public GameObject player;
+
+    [HeaderAttribute("Movement Variables")]
 	public float currentSpeed = 0.0f;
-	public float walkSpeed = 5.0f;
+	public float walkSpeed = 10.0f;
 	public float runSpeed;
 	public bool isRunning = false;
 
+    [HeaderAttribute("Rotation Variables")]
+    public float cameraRadius = 0.001f;
 
-
-	void Start(){
+    void Start(){
 		currentSpeed = walkSpeed;
 		runSpeed = walkSpeed * 1.5f;
 	}
@@ -29,24 +32,26 @@ public class PlayerMovement : MonoBehaviour {
 			isRunning = false;
 		}
 
-		//Player Movement
+		// Player Movement
 		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * currentSpeed;
 		var z = Input.GetAxis ("Vertical") * Time.deltaTime * currentSpeed;
 		transform.Translate(x, 0.0f, z);
 
-		//Player rotation
-		//Get the Screen positions of the object
-		Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+		// Player rotation
+		// Position of player
+		Vector2 playerOnScreen = Camera.main.WorldToViewportPoint (transform.position);
 
-		//Get the Screen position of the mouse
+		// Position of Mouse
 		Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
-		//Get the angle between the points
-		float angle = Mathf.Atan2(positionOnScreen.y - mouseOnScreen.y, positionOnScreen.x - mouseOnScreen.x) * Mathf.Rad2Deg;
+        if(Vector2.Distance(playerOnScreen, mouseOnScreen) >= cameraRadius)
+        {
+            // Angle between player and mouse
+            float angle = Mathf.Atan2(playerOnScreen.y - mouseOnScreen.y, playerOnScreen.x - mouseOnScreen.x) * Mathf.Rad2Deg;
 
-		//Ta Daaa
-		transform.rotation =  Quaternion.Euler (new Vector3(0f,-angle - 90,0f));
+            // Rotate
+            player.transform.rotation = Quaternion.Euler(new Vector3(0f, -angle - 90, 0f));
+        }
 
-		
 	}
 }
