d54f6eac509b69cceeb73ab4c78a2e85a3793797
Nick Stokowski <nkstokowski@gmail.com>
2018-02-27 22:42:18 -0500

    Added some comments, slightly changed the method used to get the game manager.

--- Project2/Assets/_Scripts/Player/PlayerMovement.cs
+++ Project2/Assets/_Scripts/Player/PlayerMovement.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    // GameObjects or Components that the script is dependent on
+    [HeaderAttribute("Dependencies")]
     public GameObject player;
-	GameManager gameManager;
+    public GameObject gameManagerObj;
+    private GameManager manager;
 
+
+    // Variables that affect the movement of the player
     [HeaderAttribute("Movement Variables")]
 	public float currentSpeed = 0.0f;
 	public float walkSpeed = 10.0f;
@@ -14,13 +19,21 @@ public class PlayerMovement : MonoBehaviour {
 	public bool isRunning = false;
     public Vector3 nextPosition;
 
+    // Variables that affect the rotation of the player
     [HeaderAttribute("Rotation Variables")]
     public float cameraRadius = 0.001f;
 
     void Start(){
 		currentSpeed = walkSpeed;
 		runSpeed = walkSpeed * 1.5f;
-		gameManager = GameObject.Find ("Game_Manager").GetComponent<GameManager>();
+        if (!gameManagerObj)
+        {
+            Debug.Log(name + ": No Game Manager Found");
+        }
+        else
+        {
+            manager = gameManagerObj.GetComponent<GameManager>();
+        }
 	}
 
 	void Update(){
@@ -43,9 +56,10 @@ public class PlayerMovement : MonoBehaviour {
 		transform.Translate(x, 0.0f, z);
 
 		// Player rotation
-		// Position of player
-		if(!gameManager.isPaused){
-			Vector2 playerOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+		if(!manager.isPaused){
+
+            // Position of player
+            Vector2 playerOnScreen = Camera.main.WorldToViewportPoint (transform.position);
 
 			// Position of Mouse
 			Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
--- Project2/Assets/_Scripts/Player/PlayerShooting.cs
+++ Project2/Assets/_Scripts/Player/PlayerShooting.cs
@@ -4,48 +4,70 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour {
 
+    // GameObjects or Components that the script is dependent on
+    [HeaderAttribute("Dependencies")]
+    public GameObject gameManagerObj;
+    public Transform gunEnd;
+    private GameManager manager;
+    private LineRenderer arrowLine;
+
+    // Variables that affect the shooting
+    [HeaderAttribute("Shooting Variables")]
     public int gunDamage = 1;
     public float fireRate = .25f;
     public float weaponRange = 50f;
     public float hitForce = 100f;
-    public Transform gunEnd;
     public bool singleFire;
 
     private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
-    private LineRenderer arrowLine;
     private float nextFire;
 
 	private GameManager gameManager;
 
-	// Use this for initialization
 	void Start () {
         arrowLine = GetComponent<LineRenderer>();
-		gameManager = GameObject.Find ("Game_Manager").GetComponent<GameManager>();
-	}
+        if (!gameManagerObj)
+        {
+            Debug.Log(name + ": No Game Manager Found");
+        }
+        else
+        {
+            manager = gameManagerObj.GetComponent<GameManager>();
+        }
+    }
 	
-	// Update is called once per frame
 	void Update () {
 
+        // inputActive is the state of the fire button
+        // When singleFire is active, the button must be pressed. Otherwise it can be held
+        // I don't know what would happen if singleFire were toggled on while firing
         bool inputActive = (singleFire) ? Input.GetButtonDown("Fire1") : Input.GetButton("Fire1");
 
-		if (inputActive && Time.time > nextFire && !gameManager.isPaused)
+
+        // If fire is being pressed, the weapon is ready to fire, and the game is not paused, you can fire
+		if (inputActive && Time.time > nextFire && !manager.isPaused)
         {
             nextFire = Time.time + fireRate;
 
             StartCoroutine(ShotEffect());
 
             RaycastHit hit;
-            arrowLine.SetPosition(0, gunEnd.position);
+            arrowLine.SetPosition(0, gunEnd.position); // Sets the starting position of the lineSegment to the gun end
 
+            // Check for ray collisions
             if(Physics.Raycast(gunEnd.position, gunEnd.forward, out hit, weaponRange))
             {
+                // If collision detected, set the end of the lineSegment to the collision point
                 arrowLine.SetPosition(1, hit.point);
                 EnemyHealth health = hit.collider.GetComponent<EnemyHealth>();
+
+                // If the thing that was hit has a health script, deal it some damage
                 if (health)
                 {
                     health.TakeDamage(gunDamage);
                 }
 
+                // If the thing that was hit has a rigidbody, push it? Probably will get taken out
                 if (hit.rigidbody)
                 {
                     hit.rigidbody.AddForce(-hit.normal * hitForce);
@@ -53,12 +75,17 @@ public class PlayerShooting : MonoBehaviour {
             }
             else
             {
+
+                // If there was not hit, just draw the line to the end of the weapons range
                 arrowLine.SetPosition(1, gunEnd.position + (gunEnd.forward * weaponRange));
             }
 
         }
 	}
 
+
+    // Co-routine for the effects surrounding the shot
+    // Things like audio effects for the shots can be played here
     private IEnumerator ShotEffect()
     {
         arrowLine.enabled = true;
