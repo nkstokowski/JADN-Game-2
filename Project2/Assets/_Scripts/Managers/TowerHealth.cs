2131cd242b9c9ae748ace696158155c78d9fd5f1
Juri Kiin <kiin.juri@gmail.com>
2018-03-21 22:44:43 -0400

    - Added Onscreen UI for main level - Added Game Over screen / logic - Added Pause Menu UI / logic

--- Project2/Assets/_Scripts/Managers/TowerHealth.cs
+++ Project2/Assets/_Scripts/Managers/TowerHealth.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TowerHealth : MonoBehaviour {
 
 	public int health = 100;
+	public GameObject manager;
 
 	public void TakeDamage(int damage){
 		health -= damage;
+		int score = manager.GetComponent<ScoreManager>().playerScore;
 		if(CheckForDeath()){
-			//End Game
+			if(CheckForNewHighScore(score)){
+				PlayerPrefs.SetInt("playerScore", manager.GetComponent<ScoreManager>().playerScore);
+			}
+			SceneManager.LoadScene("MainMenu");
 		}
 		else{
 			return;
@@ -24,6 +30,12 @@ public class TowerHealth : MonoBehaviour {
 			return false;
 		}
 	}
-
-
+	private bool CheckForNewHighScore(int score){
+		if(manager.GetComponent<ScoreManager>().playerScore > PlayerPrefs.GetInt("playerScore")){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 }
--- Project2/Assets/_Scripts/Player/PlayerHealth.cs
+++ Project2/Assets/_Scripts/Player/PlayerHealth.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
 	public int health = 100;
+	public GameObject manager;
+	public GameObject healthSlider;
 
 	public void TakeDamage(int damage){
 		Debug.Log ("Taking Damage!");
 		health -= damage;
+		int score = manager.GetComponent<ScoreManager>().playerScore;
 		if(CheckForDeath()){
-			//End Game
+			if(CheckForNewHighScore(score)){
+				PlayerPrefs.SetInt("playerScore", manager.GetComponent<ScoreManager>().playerScore);
+			}
+			SceneManager.LoadScene("MainMenu");
 		}
 		else{
 			return;
 		}
+	}
+
+	void Update(){
+		healthSlider.GetComponent<Slider>().value = health;
 	}
 
 	private bool CheckForDeath(){
@@ -25,4 +37,15 @@ public class PlayerHealth : MonoBehaviour {
 			return false;
 		}
 	}
+
+	private bool CheckForNewHighScore(int score){
+		if(manager.GetComponent<ScoreManager>().playerScore > PlayerPrefs.GetInt("playerScore")){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+
 }
