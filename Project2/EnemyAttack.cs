b739df3ca815868781842a62b81e42c87c1a2e2b
Ventus-Ire <wasabi.3122@gmail.com>
2018-03-28 13:36:33 -0400

    updates

---
+++ Project2/EnemyAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+	[HeaderAttribute("Enemy Type Variables")]
+	public EnemyType type;
+	int damage;
+	float attackRadius;
+
+	void Start(){
+		damage = type.damage;
+		attackRadius = type.attackRadius;
+	}
+
+	public void attack(GameObject target){
+		if(target.tag == "Target"){
+			TowerHealth tH = target.GetComponent<TowerHealth> ();
+            tH.TakeDamage(damage);
+			return;
+		}
+		if(target.tag == "Player"){
+			PlayerHealth pH = target.GetComponent<PlayerHealth> ();
+			//Debug.Log (pH.health);
+			pH.TakeDamage(damage);
+
+			return;
+		}
+	}
+
+}
